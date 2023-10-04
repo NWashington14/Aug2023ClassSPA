8be94bfc37dc99154373752ac449fa9fc4b99338
NWashington14 <nathanwashington02@gmail.com>
2023-10-03 19:20:32 -0500

    change api url to use env var

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -22,6 +23,43 @@ function afterRender() {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
+
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Pizza":
+        axios
+          .get(process.env.PIZZA_PLACE_API_URL)
+          .then(response => {
+            // Storing retrieved data in state
+            console.log(response);
+            store.Pizza.pizzas = response.data;
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
 
 router
   .on({
